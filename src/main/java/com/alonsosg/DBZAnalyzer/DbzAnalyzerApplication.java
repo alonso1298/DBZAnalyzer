f96dae0375bfc1a9e3295f708b14b98ae5ef9fa4
alonso1298 <alonsosagrerogranados@gmail.com>
2025-07-11 04:25:26 -0600

    Add class ConsumoAPI

--- src/main/java/com/alonsosg/DBZAnalyzer/DbzAnalyzerApplication.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/DbzAnalyzerApplication.java
@@ -1,13 +1,19 @@
 package com.alonsosg.DBZAnalyzer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbzAnalyzerApplication {
+public class DbzAnalyzerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbzAnalyzerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo desde Spring");
 	}
 
 }
