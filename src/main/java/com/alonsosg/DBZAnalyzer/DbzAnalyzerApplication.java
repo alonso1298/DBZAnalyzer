61185876f3f9aff2f1186bcbd32dd7dd526c69dc
alonso1298 <alonsosagrerogranados@gmail.com>
2025-07-11 04:17:22 -0600

    Initial commit

---
+++ src/main/java/com/alonsosg/DBZAnalyzer/DbzAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.alonsosg.DBZAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbzAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbzAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alonsosg/DBZAnalyzer/DbzAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alonsosg.DBZAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbzAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
