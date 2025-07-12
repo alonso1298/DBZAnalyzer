4311f9f4f228fe683dc854fbe6a84a2ecf6a9c0b
alonso1298 <alonsosagrerogranados@gmail.com>
2025-07-12 05:04:24 -0600

    Update Principal

--- src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
@@ -2,10 +2,21 @@ package com.alonsosg.DBZAnalyzer.principal;
 
 import java.util.Scanner;
 
+import com.alonsosg.DBZAnalyzer.model.DatosItems;
 import com.alonsosg.DBZAnalyzer.service.ConsumoAPI;
+import com.alonsosg.DBZAnalyzer.service.ConvierteDatos;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private final String URL_BASE = "https://dragonball-api.com/api/characters/";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraMenu(){
+        // Busca los datos generales de los personajes
+        System.out.println("Datos de los personajes");
+        String json = consumoAPI.obtenerDatos(URL_BASE);
+        DatosItems datos = conversor.obtenerDatos(json, DatosItems.class);
+        System.out.println(datos);
+    }    
 }
