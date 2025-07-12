8713736649295ecbbfcb05327591c7c428a7cae0
alonso1298 <alonsosagrerogranados@gmail.com>
2025-07-12 11:01:26 -0600

    Refactor class DatosItems

--- src/main/java/com/alonsosg/DBZAnalyzer/model/DatosItems.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/model/DatosItems.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosItems(
-    @JsonAlias("items") List<DatosPersonajes> personajes
+public record DatosItems<T>(
+    @JsonAlias("items") List<T> items
 
 ) {
 }
--- src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
@@ -3,20 +3,26 @@ package com.alonsosg.DBZAnalyzer.principal;
 // import java.util.Scanner;
 
 import com.alonsosg.DBZAnalyzer.model.DatosItems;
+import com.alonsosg.DBZAnalyzer.model.DatosPersonajes;
 import com.alonsosg.DBZAnalyzer.service.ConsumoAPI;
-import com.alonsosg.DBZAnalyzer.service.ConvierteDatos;
+// import com.alonsosg.DBZAnalyzer.service.ConvierteDatos;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Principal {
     // private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private final String URL_BASE = "https://dragonball-api.com/api/characters/";
-    private ConvierteDatos conversor = new ConvierteDatos();
+    ObjectMapper objectMapper = new ObjectMapper();
+    // private ConvierteDatos conversor = new ConvierteDatos();
 
-    public void muestraMenu(){
+    public void muestraMenu() throws JsonMappingException, JsonProcessingException{
         // Busca los datos generales de los personajes
         System.out.println("Datos de los personajes");
         String json = consumoAPI.obtenerDatos(URL_BASE);
-        DatosItems datos = conversor.obtenerDatos(json, DatosItems.class);
-        System.out.println(datos);
+        DatosItems<DatosPersonajes> datosPersonajes = objectMapper.readValue(json, new TypeReference<DatosItems<DatosPersonajes>>() {});
+        System.out.println(datosPersonajes);
     }    
 }
