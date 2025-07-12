d8bc1d8283caa222753b82de66b62d58916f658b
alonso1298 <alonsosagrerogranados@gmail.com>
2025-07-12 04:50:49 -0600

    Add variables to class Principal

--- src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
@@ -1,5 +1,11 @@
 package com.alonsosg.DBZAnalyzer.principal;
 
+import java.util.Scanner;
+
+import com.alonsosg.DBZAnalyzer.service.ConsumoAPI;
+
 public class Principal {
-    
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String URL_BASE = "https://dragonball-api.com/api/characters/";
 }
