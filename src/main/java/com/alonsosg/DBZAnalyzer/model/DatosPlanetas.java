d69fa08f238b2c7c6c69d0a54f8a49d3dbc5b0a6
alonso1298 <alonsosagrerogranados@gmail.com>
2025-07-13 19:37:26 -0600

    Se agregan los endopoints

--- src/main/java/com/alonsosg/DBZAnalyzer/model/DatosPlanetas.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/model/DatosPlanetas.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosPlanetas(
     @JsonAlias("id") Integer id,
-    @JsonAlias("nanme") String nombrePlaneta,
-    @JsonAlias("isDestroyed") Double estaDestruido,
+    @JsonAlias("name") String nombrePlaneta,
+    @JsonAlias("isDestroyed") Boolean estaDestruido,
     @JsonAlias("description") String descripcion
 ){}
--- src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
+++ src/main/java/com/alonsosg/DBZAnalyzer/principal/Principal.java
@@ -4,6 +4,7 @@ package com.alonsosg.DBZAnalyzer.principal;
 
 import com.alonsosg.DBZAnalyzer.model.DatosItems;
 import com.alonsosg.DBZAnalyzer.model.DatosPersonajes;
+import com.alonsosg.DBZAnalyzer.model.DatosPlanetas;
 import com.alonsosg.DBZAnalyzer.service.ConsumoAPI;
 // import com.alonsosg.DBZAnalyzer.service.ConvierteDatos;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -14,15 +15,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Principal {
     // private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-    private final String URL_BASE = "https://dragonball-api.com/api/characters/";
+    private final String URL_BASE = "https://dragonball-api.com/api/";
     ObjectMapper objectMapper = new ObjectMapper();
     // private ConvierteDatos conversor = new ConvierteDatos();
 
     public void muestraMenu() throws JsonMappingException, JsonProcessingException{
         // Busca los datos generales de los personajes
         System.out.println("Datos de los personajes");
-        String json = consumoAPI.obtenerDatos(URL_BASE);
+        String json = consumoAPI.obtenerDatos(URL_BASE + "characters/");
         DatosItems<DatosPersonajes> datosPersonajes = objectMapper.readValue(json, new TypeReference<DatosItems<DatosPersonajes>>() {});
         System.out.println(datosPersonajes);
+
+        System.out.println("Datos de los planetas");
+        json = consumoAPI.obtenerDatos(URL_BASE + "planets/");
+        DatosItems<DatosPlanetas> datosPlanetas = objectMapper.readValue(json, new TypeReference<DatosItems<DatosPlanetas>>() {});
+        System.out.println(datosPlanetas);
     }    
 }
